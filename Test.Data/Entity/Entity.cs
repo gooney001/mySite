709025d735518a43b510c88b975a4a85f7ae0153
goony1 <1178332920@qq.com>
2018-07-25 10:34:42 +0800

    20180725

---
+++ Test.Data/Entity/Entity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test.Data.Entity
+{
+    public abstract class Enity: Entity<int>
+    {
+
+    }
+    public abstract class Entity<TPrimaryKey>
+    {
+        public virtual TPrimaryKey Id { get; set; }
+    }
+}
---
+++ Test.Data/SysDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Test.Data.Entity;
+
+namespace Test.Data
+{
+    public class SysDbContext:DbContext
+    {
+        public SysDbContext(DbContextOptions<SysDbContext> options) : base(options) { }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseMySQL("Server=127.0.0.1;database=sysdb;uid=root;pwd=123456;");
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
